15af59e0c7455f72c83b68b670b6edeaf7e72d79
Magikis <kulczakkacper@gmal.com>
2018-11-12 18:27:44 +0100

    output logging

--- main.py
+++ main.py
@@ -4,22 +4,29 @@ import numpy
 from snakeoil.snakeoil import Client
 # from agents.lineFolowerContinus import drive
 # from agents.lineForowerDiscreate import drive as driveDescrete
+from settings import settings
 import agents.line_folower as line_folower
 import agents.lineForowerDiscreate as discreate
 import agents.lineFolowerContinus as continous
+import datetime
+
 
 def drivingLoop(drivingFunc):
     C = Client()
-    for step in range(C.maxSteps, 0, -1):
-        C.get_servers_input()
-        data = C.S.d
-        data.update(angle=numpy.degrees(data['angle']))
+    import csv
+    with open(f'logs/{datetime.datetime.now()}', 'w') as f:
+        writer = csv.DictWriter(f, settings['log_keys'])
+        writer.writeheader()
+        while True:
+            C.get_servers_input()
+            data = C.S.d
+            data.update(angle=numpy.degrees(data['angle']))
+            response = drivingFunc(data, 0)
+            C.R.d = {**response}
+            sys.stdout.flush()
+            writer.writerow({**data, **response})
 
-        response = drivingFunc(data, step)
-        C.R.d = {**response}
-        sys.stdout.flush()
-
-        C.respond_to_server()
+            C.respond_to_server()
     C.shutdown()
 
 
