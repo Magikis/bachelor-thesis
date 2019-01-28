3421631213cb890ea4f37d609847441c5048fe24
Magikis <kulczakkacper@gmal.com>
2019-01-28 16:23:20 +0100

    Only Future Work left

--- speeed_limits_learner.py
+++ speeed_limits_learner.py
@@ -30,7 +30,7 @@ def bisect(f, a, times, alpha=150):
 
 
 def main(**args):
-    table = np.array([40.] * 8)
+    table = np.array([40.] * 16)
     hist = []
     for i, x in enumerate(table):
         speed_limits = table.copy()
